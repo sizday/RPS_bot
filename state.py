15212a7e75ca12145cbc7f7e90ecf30eb201a387
dsizo <dsizov1999@mail.ru>
2020-05-11 22:36:59 +0300

    state yes/no

--- state.py
+++ state.py
@@ -4,6 +4,3 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class Game(StatesGroup):
     entering = State()
     choosing = State()
-    new_game = State()
-    no_game = State()
-    yes_game = State()
