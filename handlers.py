09c39d5bf0606eaaf9e24b8ad54e71a152ef59ab
dsizo <dsizov1999@mail.ru>
2020-05-12 12:18:21 +0300

    admin_panel

--- handlers.py
+++ handlers.py
@@ -17,11 +17,12 @@ db = database.DBCommands()
 async def register_user(message: types.Message):
     chat_id = message.from_user.id
     user = await db.add_new_user()
-    text = f'Приветствую вас, {user.full_name}'
+    text = f'Приветствую вас, {user.full_name}' \
+           f'Ваш id = {message.from_user.id}'
     await bot.send_message(chat_id, text)
 
 
-@dp.message_handler(commands=["count"])
+@dp.message_handler(user_id=admin_id, commands=["count"])
 async def count_user(message: types.Message):
     chat_id = message.from_user.id
     count_users = await db.count_users()
